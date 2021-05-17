cf07dc7aa0183e93404e40fc45ae7e2c7b335e75
slamdunk7575 <slamdunk7575@gmail.com>
2021-05-17 23:51:35 +0900

    (feat) 프로젝트 환경 구성

---
+++ src/test/java/me/toy/atdd/subwayservice/SubwayApplicationTests.java
@@ -0,0 +1,13 @@
+package me.toy.atdd.subwayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
